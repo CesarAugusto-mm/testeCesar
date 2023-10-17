97ce41abdeef631c4a6263af837e091b8e4f7341
CesarAugusto-mm <Cesarmmarinho019@hotmail.com>
2023-10-16 23:01:57 -0300

    projeto Spring bicicleta

---
+++ src/main/java/com/bicicleta/bicicletaCesar/BicicletaCesarApplication.java
@@ -0,0 +1,13 @@
+package com.bicicleta.bicicletaCesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicletaCesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicletaCesarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bicicleta/bicicletaCesar/BicicletaCesarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bicicleta.bicicletaCesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicicletaCesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
